7887d4f1ff2156eabfc9e046a543794ed1dc0419
reportJNG <hamzaremali10@gmail.com>
2026-02-22 03:18:16 -0800

    created first sign up action to sign in the user

---
+++ Backend/Server/SignupAction.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { SingupType } from "@/Frontend/Schemas/Signup";
+import { prisma } from "@/lib/prisma";
+
+export async function SignupAction(data: SingupType) {
+  if (!data) {
+    return { error: "Failed Data" };
+  }
+  const { email, name, password, confirmPassword } = data;
+  if (!email || !name || !password || !confirmPassword) {
+    return { error: "All fields are required" };
+  }
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    return { error: "Invalid email format" };
+  }
+  if (!/^[A-Za-z0-9]+$/.test(name)) {
+    return { error: "Name must contain only letters and numbers" };
+  }
+  if (
+    !/^[A-Za-z0-9]+$/.test(password) ||
+    !/^[A-Za-z0-9]+$/.test(confirmPassword)
+  ) {
+    return { error: "Password must contain only letters and numbers" };
+  }
+  if (password !== confirmPassword) {
+    return { error: "Password must be same" };
+  }
+  if (password.length < 8 || password.length > 16)
+    return { error: "Password must be between 8 and 16 characters" };
+
+  const exist = await prisma.users.findUnique({
+    where: { email },
+  });
+  if (!exist) {
+    return { error: "Email used before" };
+  }
+  try {
+    const putdata = await prisma.users.create({
+      data: { email, name, password },
+    });
+  } catch (err) {
+    return { error: "Failed to fetch" };
+  }
+}
