fad45f4eee61747c50c7747524d6b4db192266a7
reportJNG <hamzaremali10@gmail.com>
2026-02-24 06:53:19 -0800

    updated profile a bit of logic and ui 100% get data for ui logic

--- Frontend/Pages/Profile.tsx
+++ Frontend/Pages/Profile.tsx
@@ -1,20 +1,39 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Topbar from "@/Frontend/components/Myui/Topbar";
 import About from "@/Frontend/components/Myui/About";
 import { UserCircle2Icon } from "lucide-react";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardHeader,
   CardTitle,
 } from "../components/ui/card";
+import { GetInfoUser } from "@/Backend/Server/GetInfoUser";
+import { users } from "@/lib/generated/prisma";
+import { Button } from "../components/ui/button";
 
 export default function Profile() {
   const [settings, setSettings] = useState<boolean>(false);
   const [terms, setTerms] = useState<boolean>(false);
-
+  const [userdata, setUserdata] = useState<users>({
+    email: "",
+    id: "",
+    created_at: null,
+    name: "",
+    password: "",
+  });
+  useEffect(() => {
+    const getdata = async () => {
+      const result = await GetInfoUser();
+      if (result.success) {
+        setUserdata(result.success);
+      }
+    };
+    getdata();
+  }, []);
   return (
     <div>
       <header>
@@ -40,9 +59,20 @@ export default function Profile() {
                 <CardTitle>User Information</CardTitle>
               </CardHeader>
               <CardContent>
-                <input type="text" disabled />
-                <input type="text" disabled />
+                <input type="text" disabled value={userdata?.email} />
+                <input type="text" disabled value={userdata?.name ?? ""} />
               </CardContent>
+              <CardDescription>
+                <div>
+                  <Button aria-label="Reset Password" title="Reset Password">
+                    Reset Password
+                  </Button>
+
+                  <Button aria-label="Edit Profile" title="Edit Profile">
+                    Edit Profile
+                  </Button>
+                </div>
+              </CardDescription>
             </Card>
           </div>
         </div>
