5e00ff56f92bbfa656becca7175fcc8cc3e80af0
reportJNG <hamzaremali10@gmail.com>
2026-02-23 16:21:14 -0800

    bit fixed in token checker

--- Backend/Server/TokenChecker.ts
+++ Backend/Server/TokenChecker.ts
@@ -1,20 +1,18 @@
 "use server";
 import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export async function TokenChecker() {
   const tokenexist = (await cookies()).get("token")?.value;
   if (!tokenexist) {
     return { error: "Failed to fetch" };
   }
-
   try {
     const decoding = jwt.verify(tokenexist, process.env.JWT_SECRET!) as {
       userId: string;
     };
     return { success: decoding.userId };
-  } catch (err) {
+  } catch {
     return { error: "Failed to fetch" };
   }
 }
