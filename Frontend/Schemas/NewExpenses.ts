62a3564cb789fef97f105a3fb4fa6ba757c1e682
reportJNG <hamzaremali10@gmail.com>
2026-03-01 17:37:50 -0600

    create nice zod handler for new expenses

---
+++ Frontend/Schemas/NewExpenses.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const ExpensesSchema = z.object({
+  name: z
+    .string()
+    .max(10)
+    .min(1)
+    .regex(/^[a-zA-Z0-9]+$/, "Name must only contain letters and numbers"),
+  amount: z.number().max(1000000).min(0),
+  type: z.enum(["income", "expense"]),
+  category: z
+    .string()
+    .max(20)
+    .min(2)
+    .regex(/^[a-zA-Z]+$/, "Must only contain letters"),
+  description: z
+    .string()
+    .max(100)
+    .regex(/^[a-zA-Z]*$/, "Must only contain letters")
+    .optional(),
+});
