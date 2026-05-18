718d508dcca05d3a51abbdfadd682718d6fe6af1
reportJNG <hamzaremali10@gmail.com>
2026-05-18 07:27:25 -0700

    relaying expense components to  all expenses page

--- Frontend/Pages/Expenses.tsx
+++ Frontend/Pages/Expenses.tsx
@@ -6,17 +6,20 @@ import Settings from "../components/Myui/Settings";
 import Terms from "../components/Myui/Terms";
 import Topbar from "../components/Myui/Topbar";
 import { useState } from "react";
-import { users } from "@/lib/generated/prisma";
+import { expenses, users } from "@/lib/generated/prisma";
 import { useEffect } from "react";
 import { GetInfoUser } from "@/Backend/Server/GetInfoUser";
 import { useRouter } from "next/navigation";
-import { PenSquareIcon } from "lucide-react";
+import { Eye, EyeClosedIcon, PenSquareIcon } from "lucide-react";
 import Expense from "@/Frontend/components/Myui/Expense";
 export default function Expenses() {
   const [settings, setSettings] = useState<boolean>(false);
   const [terms, setTerms] = useState<boolean>(false);
   const [NewExpenses, setNewExpenses] = useState<boolean>(false);
   const routes = useRouter();
+  const [hiddenitems, SetHiddenitems] = useState<boolean>(false);
+  const editexpense = (id: string, usid: string) => {};
+
   const [user, setUser] = useState<users>({
     id: "",
     name: "",
@@ -34,6 +37,9 @@ export default function Expenses() {
     };
     call();
   }, [routes]);
+  //logic to handle the each expene
+  const [expense, SetExpense] = useState<expenses[]>([]);
+  const delexpense = (id: string, usid: string) => {};
 
   return (
     <>
@@ -56,7 +62,21 @@ export default function Expenses() {
             {/**all expenses*/}
             <div>
               <h1 className="text-2xl font-bold mb-4">All Expenses</h1>
-              <Expense />
+              <div>
+                <button onClick={() => SetHiddenitems((prev) => !prev)}>
+                  {hiddenitems ? <EyeClosedIcon /> : <Eye />}
+                </button>
+              </div>
+              {expense &&
+                expense.map((item) => (
+                  <div key={item.id}>
+                    <Expense
+                      item={item}
+                      edit={() => editexpense(item.id, item.user_id)}
+                      del={() => delexpense(item.id, item.user_id)}
+                    />
+                  </div>
+                ))}
             </div>
             {/**button creation */}
             <button onClick={() => setNewExpenses((prev) => !prev)}>
--- Frontend/components/Myui/Expense.tsx
+++ Frontend/components/Myui/Expense.tsx
@@ -16,28 +16,39 @@ interface exp {
 export default function Expense({ item, edit, del }: exp) {
   return (
     <div>
-      <div>
-        <Calendar />{" "}
-        <strong>{new Date(item.date).toLocaleDateString("fr-FR")}</strong>
-      </div>
-      <div>
-        {item.type === "income" ? <PlusCircleIcon /> : <MinusCircleIcon />}{" "}
-        <strong>{item.name}</strong>
-      </div>
-      <div>
-        <div>
-          {Number(item.amount).toFixed(2)}
-          <DollarSign />
-        </div>
-        <div>
-          <button onClick={() => edit(item.id, item.user_id)}>
-            <EditIcon />
-          </button>
-          <button onClick={() => del(item.id, item.user_id)}>
-            <Trash2 />
-          </button>
-        </div>
-      </div>
+      {item && (
+        <>
+          <div>
+            <Calendar />{" "}
+            <strong>{new Date(item.date).toLocaleDateString("fr-FR")}</strong>
+          </div>
+          <div>
+            {item.type === "income" ? <PlusCircleIcon /> : <MinusCircleIcon />}{" "}
+            <strong>{item.name}</strong>
+          </div>
+          <div>
+            <div>
+              {Number(item.amount).toFixed(2)}
+              <DollarSign />
+            </div>
+            <div>
+              <button onClick={() => edit}>
+                <EditIcon />
+              </button>
+              <button onClick={() => del}>
+                <Trash2 />
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+      {!item && (
+        <>
+          <div>
+            <strong>Empty</strong>
+          </div>
+        </>
+      )}
     </div>
   );
 }
