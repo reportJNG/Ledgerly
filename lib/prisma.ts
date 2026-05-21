44b0e24c0b491658fc4b245face15b62689fa51d
reportJNG <hamzaremali10@gmail.com>
2026-05-20 17:53:20 -0700

    dw

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,15 @@
-import { PrismaClient } from "../lib/generated/prisma";
+import { PrismaClient } from "./generated/prisma";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
